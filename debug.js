d0e6959a829c7b457a2ffcfbc4a5ad537d5c736f
gayusra <163907580+gayusra@users.noreply.github.com>
2024-08-25 21:07:09 -0700

    second commit

--- debug.js
+++ debug.js
@@ -1,5 +1,15 @@
+
+
+/*Debugging in Node.js is the process of finding and fixing issues or bugs in your code. Node.js provides several tools and techniques for debugging, including using the built-in debugger, console statements, and external debugging tools like the Chrome DevTools.
+*/
+
+
+
+
+
+
 // app.js
-function add(a, b) {
+/*function add(a, b) {
     return a + b;
 }
 
@@ -24,3 +34,34 @@ function main() {
 }
 
 main();
+*/
+
+console.log('debug testing')
+function add(a,b){
+    return a+b
+}
+ debugger;
+
+console.log("debug test")
+
+add(2,2)
+
+// app.js
+/*function add(a, b) {
+    console.log("Values of a and b:", a, b); // This will print the values of a and b
+    return a + b;
+}
+
+const result = add(5, 3);
+console.log("Result:", result); // This will print the result of the addition*/
+
+
+// app.js
+/*function add(a, b) {
+    debugger; // Execution will pause here
+    return a + b;
+}
+
+const result = add(5, 3);
+console.log("Result:", result);
+*/
